ff14c3fea490ac9a41beb0de6a7850ef22e77a70
Naveen Dewangan <naveen2000dev.bd@gmail.com>
2023-03-06 20:34:37 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+
+import {logo} from './assets';
+import { Home, CreatePost } from './views';
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
