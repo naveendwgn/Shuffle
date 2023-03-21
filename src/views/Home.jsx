ed285fb04f9eba9b5c2a3ccb8bbd2af4c79adf29
Naveen Dewangan <naveen2000dev.bd@gmail.com>
2023-03-21 20:58:39 +0530

    button successfully added

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect} from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 
 import { Loader, Card, FormField } from '../components';
 import CreatePost from './CreatePost';
+
 
 
 const RenderCards = ({ data, title}) => {
@@ -24,6 +25,8 @@ const Home = () => {
   const [allposts, setAllPosts] = useState([]);
 
   const [searchText, setSearchText] = useState('');
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -48,15 +51,19 @@ const Home = () => {
     fetchPosts();
   }, []);
 
+
   return (
     <section className="max-w7x1 mx-auto">
       <div className= "flex flex-col items-center justify-center mt-28">
         <h1 className="mt-12 font-bold text-[#1b1719] text-[80px]">Discover the magic of Shuffle,</h1>
         <h3 className="text-[#1b1719] text-[30px]">the app that turns your words into incredible images.</h3>
         <p className='mt-1 text-[#4b5563] text-[15px]'>With DALL-E technology, your ideas will be brought to life like never before.</p>
-          <button className="mt-10 bg-[#1b1719] text-[#fff] text-[15px] py-2 px-4 rounded-xl hover:bg-[#ea3e3e]">
-            <a href="/create-post">Create</a>
-          </button>
+        
+        <button className="mt-8 bg-[#1b1719] text-white py-2 px-4 rounded-full" onClick={() => navigate('/create-post')}>
+          Create
+        </button>
+      
+        
       </div>
 {/*
       <div className="mt-16">
