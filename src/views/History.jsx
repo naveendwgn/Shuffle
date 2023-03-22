939e1225d1df49105a95c3acaecebf2e7ed5db19
Naveen Dewangan <naveen2000dev.bd@gmail.com>
2023-03-22 20:58:55 +0530

    update

--- src/views/History.jsx
+++ src/views/History.jsx
@@ -32,7 +32,7 @@ function History() {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://brick-red-lamb-tux.cyclic.app/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
