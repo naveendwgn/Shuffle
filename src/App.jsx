7981e84b90f63f3d215204e2e875954c2575fec7
Naveen Dewangan <naveen2000dev.bd@gmail.com>
2023-03-28 00:00:16 +0530

    Styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,12 @@ import { Home, CreatePost, History } from './views';
 const App = () => {
   return (
     <BrowserRouter>
-       <header className="w-full flex items-center bg-[#1E1C1C] sm:px-8 px-4 py-2">
+       <header className="w-full flex items-center bg-[#000000] sm:px-8 px-4 py-2">
           <Link to="/" className="mr-auto">
-            <h1 className="text-2xl text-[#ffffff]">Shuffle</h1>
+            <a href='https://shuffle-eosin.vercel.app/'>
+            <h1 className="text-2xl text-[#ffffff]"
+            >Shu<span class="text-[#ff4545]">ff</span>le</h1>
+            </a>
           </Link>
           <div className="ml-auto flex">
             <Link to="/history" className="text-lg text-[#ffffff] px-5 py-2 border rounded-2xl mr-2">
@@ -20,15 +23,19 @@ const App = () => {
             </Link>
           </div>
         </header>
-      <main className="sm:p-8 px-4 py-8 w-full bg-[#1E1C1C] min-h-[calc(100vh-73px)]">
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#000000] min-h-[calc(100vh-73px)]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/history" element={<History />} />
           <Route path="/create-post" element={<CreatePost />} />
         </Routes>
         </main>
-          <footer className="w-full bg-[#1E1C1C] py-4 px-4 text-center text-[#1b1719]">
-            <p className="text-sm text-white">© 2023 Shuffle. All rights reserved.</p>
+          <footer className="w-full bg-[#000000] py-4 px-4 text-center text-[#1b1719]">
+            <div className="flex flex-col items-center">
+            <p className="text-sm text-white">Powered by <a className=' text-sm font-semibold text-[#ff4545]'
+            href="https://openai.com/">Open AI</a> © 2023 Shuffle.</p>
+            <p className="text-sm text-white">Made with ❤️ by <span className='text-[#ff4545]'>Naveen Dewangan</span></p>
+            </div>
           </footer>
     </BrowserRouter>
   );
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className= "flex flex-col items-center justify-center mt-28">
         <h1 className="mt-12 font-semibold text-[#fdfdff] text-[70px]">Discover the magic of Shuffle,</h1>
         <h3 className="text-[#ffffff] text-[30px]">the app that turns your words into incredible images.</h3>
-        <p className='mt-2 text-[#eeeeee] text-[15px]'>With DALL-E technology, your ideas will be brought to life like never before.</p>
+        <p className='mt-2 text-[#eeeeee] text-[15px]'>With DALL-E technology, your imagination will be brought to life like never before.</p>
         
         <button className="mt-9 bg-[#ff4545] text-[#ffffff] py-3 px-7 rounded-full hover:bg-[#ea3e3e] focus:outline-none" onClick={() => navigate('/create-post')}>
           Create
